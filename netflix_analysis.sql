658836b75ef6c3f2df799178004bd04c004b641c
Letícia Almeida <leticiaalmeida@MacBook-Air-de-Leticia.local>
2024-03-08 20:22:37 -0300

    first commit

---
+++ netflix_analysis.sql
@@ -0,0 +1,236 @@
+SELECT * FROM treino.netflix_titles;
+
+
+-- Contando a quantidade de títulos
+SELECT
+	COUNT(*)
+FROM netflix_titles;
+
+
+-- Contando a quantidade de obras lançadas e adicionadas à plataforma por ano
+SELECT
+    year,
+    SUM(count_added_titles) AS total_added_titles,
+    SUM(count_release_titles) AS total_release_titles
+FROM (
+    SELECT
+        YEAR(STR_TO_DATE(date_added, '%M %d, %Y')) AS year,
+        COUNT(*) AS count_added_titles,
+        NULL AS release_year,
+        0 AS count_release_titles
+    FROM netflix_titles
+    GROUP BY year
+
+    UNION ALL
+
+    SELECT
+        release_year AS year,
+        0 AS count_added_titles,
+        release_year,
+        COUNT(*) AS count_release_titles
+    FROM netflix_titles
+    GROUP BY release_year
+) AS subquery
+GROUP BY year
+ORDER BY total_added_titles DESC;
+
+
+-- Contando a quantidade de filmes e séries produzidos
+SELECT
+    type,
+    COUNT(type) AS 'type_of_title'
+FROM 
+	netflix_titles
+GROUP BY
+    type; -- usar o mesmo nome da coluna original
+
+
+
+-- Contando a quantidade de filmes e séries produzidos por ano    
+SELECT
+    release_year,
+    type,
+    COUNT(type) AS type_of_title
+FROM 
+	netflix_titles
+GROUP BY 
+	release_year, 
+    type
+ORDER BY release_year DESC;
+
+
+
+
+-- Contando a quantidade de obras por diretor
+SELECT
+	director,
+    COUNT(director) AS 'total_titles'
+FROM netflix_titles
+GROUP BY director
+ORDER BY total_titles DESC;
+
+
+-- Separando a duração das obras entre número e unidade
+SELECT
+    duration,
+    SUBSTRING_INDEX(duration, ' ', 1) AS duration_number,
+    SUBSTRING(duration, LOCATE(' ', duration) + 1) AS duration_unit
+FROM
+    netflix_titles;
+
+
+-- Utilizando a consulta anterior como subquerie    
+SELECT
+		year,
+        (SELECT
+			duration,
+			SUBSTRING_INDEX(duration, ' ', 1) AS duration_number,
+			SUBSTRING(duration, LOCATE(' ', duration) + 1) AS duration_unit
+		FROM
+			netflix_titles)
+FROM netflix_titles;
+
+
+-- Extraindo a duração de filmes e séries
+SELECT
+    type,
+    SUBSTRING_INDEX(duration, ' ', 1) AS duration_number,
+    SUBSTRING(duration, LOCATE(' ', duration) + 1) AS duration_unit
+FROM
+    netflix_titles;
+
+
+-- Criando uma view para armazenar os dados de duração dos títulos
+CREATE VIEW duration AS
+SELECT
+    type,
+    SUBSTRING_INDEX(duration, ' ', 1) AS duration_number,
+    SUBSTRING(duration, LOCATE(' ', duration) + 1) AS duration_unit
+FROM
+    netflix_titles;
+    
+SELECT * FROM duration;
+
+
+-- Exibindo os tipos de unidade de medida
+SELECT 
+	duration_unit,
+	COUNT(duration_unit) AS units
+FROM
+	duration
+GROUP BY duration_unit;
+  
+  
+  
+-- Média de duração dos filmes em minutos
+SELECT
+	type,
+    AVG(duration_number) AS 'avg_duration'
+FROM duration
+WHERE type = 'Movie'
+GROUP BY type;
+
+
+-- Média de duração das séries em temporadas --> foi necessário fazer filtrando a uniade por estarem com escritas diferentes
+SELECT
+	type,
+    duration_unit,
+    AVG(duration_number) AS 'avg_duration'
+FROM duration
+WHERE duration_unit LIKE 'Season%'
+GROUP BY type, duration_unit;
+
+
+
+
+-- Contando a quantidade de séries com apenas uma temporada
+SELECT
+	COUNT(duration_number) AS 'count_duration_number'
+FROM duration
+WHERE duration_number = 1;
+
+
+
+-- Expondo os títulos exibidos no Brazil
+SELECT
+	country,
+    type,
+    title
+FROM
+	netflix_titles
+WHERE country LIKE '%Brazil%'
+GROUP BY type, country, title;
+
+
+SELECT
+	description
+FROM netflix_titles;
+
+
+
+-- Exibindo países com mais títulos com a palavra 'polícia' na descrição
+SELECT
+	country,
+	COUNT(description) AS count_description
+FROM
+	netflix_titles
+WHERE description LIKE '%police%'
+GROUP BY country
+ORDER BY count_description DESC;
+
+
+-- Exibindo países com mais títulos listados em categorias de ação
+SELECT
+	country,
+    COUNT(listed_in) AS count_listed_in
+FROM 
+	netflix_titles
+WHERE
+	listed_in LIKE '%Action%'
+GROUP BY country
+ORDER BY count_listed_in DESC;
+
+
+
+-- Exibindo países com mais títulos listados em categorias de documentários
+SELECT
+	country,
+    COUNT(listed_in) AS count_listed_in
+FROM 
+	netflix_titles
+WHERE
+	listed_in LIKE '%Documentaries%'
+GROUP BY country
+ORDER BY count_listed_in DESC;
+
+
+
+SELECT
+	country,
+    COUNT(listed_in) AS count_listed_in
+FROM 
+	netflix_titles
+GROUP BY country
+HAVING count_listed_in LIKE '%Documentaries%'
+ORDER BY count_listed_in DESC;
+
+    
+
+-- Expondo títulos exibidos no Brazil
+SELECT
+    country,
+    type,
+    COUNT(description) AS 'count_description'
+FROM
+    netflix_titles
+WHERE description LIKE '%police%'
+GROUP BY type, country
+ORDER BY count_description DESC;
+
+
+
+
+
+
+
+
